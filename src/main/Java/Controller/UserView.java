46c00630614ef5ab9e8f494aedc681e21b68a994
Geovane <geovanegasparello@gmail.com>
2017-10-09 15:30:09 -0300

    Tela e controller de login e cadastro

---
+++ src/main/Java/Controller/UserView.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+public class UserView {
+
+        private String email;
+        private String senha;
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getSenha() {
+            return senha;
+        }
+
+        public void setSenha(String senha) {
+            this.senha = senha;
+        }
+
+        public void save() {
+            FacesContext.getCurrentInstance();//.addMessage(null,
+                    //new FacesMessage("Welcome " + email + " " + senha));
+        }
+
+}
