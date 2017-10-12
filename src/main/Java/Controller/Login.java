d7968d2a000bfe4524e984ebe9a1b0e89f750372
Francislaine Ortega Monteiro <faine.ormon@gmail.com>
2017-10-12 14:56:05 -0300

    Inclusão das telas
    Inclusão das telas de login, cadastro e homeRH + controler necessário

---
+++ src/main/Java/Controller/Login.java
@@ -0,0 +1,26 @@
+package Controller;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Login {
+
+    private String email;
+    private String senha;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
