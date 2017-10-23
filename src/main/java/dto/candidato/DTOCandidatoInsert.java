cd8b4c20792d3e5b9cab475beef3b052c8ff935d
faine ormon <faine.ormon@gmail.com>
2017-10-23 14:48:52 -0200

    remoção do rodapé do template

--- src/main/java/dto/candidato/DTOCandidatoInsert.java
+++ src/main/java/dto/candidato/DTOCandidatoInsert.java
@@ -1,4 +1,23 @@
 package dto.candidato;
 
+import java.util.List;
+
+import model.Prova;
+
 public class DTOCandidatoInsert {
+    private final String nome;
+    private final String email;
+    private final String telefone;
+    private final String redesocial;
+    private final Boolean contratado;
+    private final List<Prova> prova;
+
+    public DTOCandidatoInsert (String nome, String email, String telefone, String redesocial, Boolean contratado, List<Prova> prova) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.redesocial = redesocial;
+        this.contratado = contratado;
+        this.prova = prova;
+    }
 }
