c00f2081db67d82a16364e6ad76df727f6ad7dfa
Gabriel Galego <gabrielgalego25@gmail.com>
2017-10-25 15:21:28 -0200

    alteração feita no candidato controller e no DAO, Criterios tambem

--- src/main/java/dao/CandidatoDAO.java
+++ src/main/java/dao/CandidatoDAO.java
@@ -1,4 +1,45 @@
 package dao;
 
+import dto.candidato.DTOCandidatoInsert;
+import dto.candidato.DTOCandidatoUpdate;
+import model.Candidato;
+import model.Linguagem;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Named
 public class CandidatoDAO {
+
+    @Inject
+    private EntityManager manager;
+
+    public List<Candidato> showCandidato () {
+        return manager.createQuery("select f from Candidato f where nome = ").getResultList();
+    }
+
+    public String novoCandidato(dto.candidato.DTOCandidatoInsert request){
+        Candidato candidato = new Candidato();
+
+        candidato.setNome(request.getNome());
+        candidato.setEmail(request.getEmail());
+        candidato.setTelefone(request.getTelefone());
+        candidato.setEmail(request.getEmail());
+        candidato.setRede_social(request.getRede_social());
+        candidato.setContratado(true);
+
+
+        manager.getTransaction().begin();
+        manager.persist(Candidato);
+        manager.getTransaction().commit();
+
+        return "Realizado com sucesso";
+    }
+
+    public String UpdateCandidato (DTOCandidatoUpdate request) {
+
+        return "Realizado com sucesso";
+    }
 }
--- src/main/java/dto/criterios/DTOCriteriosDelete.java
+++ src/main/java/dto/criterios/DTOCriteriosDelete.java
@@ -3,12 +3,16 @@ package dto.criterios;
 public class DTOCriteriosDelete {
 
     private final String descricao;
+    private final Integer id;
 
-    public DTOCriteriosDelete(String descricao) {
+    public DTOCriteriosDelete(String descricao, Integer id) {
         this.descricao = descricao;
+        this.id = id;
     }
 
     public String getDescricao() {
         return descricao;
     }
+
+    public Integer getId() { return id; }
 }
